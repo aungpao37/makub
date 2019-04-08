7db5058c798ee55e49d28ff67f2e733437b93203
aungpao37 <aungpao37@hotmail.com>
2019-04-08 21:22:12 +0700

    Updates

---
+++ precache-manifest.74d80e0665d0731cee5d7aacfe3a4d30.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "c478b3b9b5c7864c02b5",
+    "url": "/makub/static/js/runtime~main.c40db0b1.js"
+  },
+  {
+    "revision": "6989031da2680f162f96",
+    "url": "/makub/static/js/main.5f4e7a95.chunk.js"
+  },
+  {
+    "revision": "9eee67cf782a38d35aeb",
+    "url": "/makub/static/js/2.d6a534e5.chunk.js"
+  },
+  {
+    "revision": "6989031da2680f162f96",
+    "url": "/makub/static/css/main.305bb8ac.chunk.css"
+  },
+  {
+    "revision": "d7801010f929310282f1611ad17cd249",
+    "url": "/makub/index.html"
+  }
+];
